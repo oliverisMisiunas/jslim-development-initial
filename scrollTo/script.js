ba6bb04812741923e4b3ee9b5037ca04ffbbbc78
ausytesWebsite <100309052+ausytesWebsite@users.noreply.github.com>
2022-03-10 14:57:18 +0200

    third add

--- scrollTo/script.js
+++ scrollTo/script.js
@@ -1,21 +1,22 @@
-const butke = document.getElementsByClassName('pirmas')
+const jscrollto = document.getElementsByClassName('jscrollto')
 
-document.getElementById('vapse-taip').style.color = 'red'
+document.getElementById('vapse-taip').style.color = 'red'  //delete this
 
-for (let i = 0; i < butke.length; i++) {
-    butke[i].addEventListener('click', () => {
-    let classNames = butke[i].className.split(' ');
-    let elementas = document.getElementById(`${classNames[1]}`);
+for (let i = 0; i < jscrollto.length; i++) {
+    jscrollto[i].addEventListener('click', () => {
+    let classNames = jscrollto[i].className.split(' ');
+    let scrPos = classNames.indexOf('jscrollto');
+    let elementas = document.getElementById(`${classNames[scrPos + 1]}`);
     let elemOff = elementas.offsetTop;
 
-    if (classNames[2] == 'jsmooth') {
-        if (classNames[3] == 'jsetpx') {
-            window.scrollTo({left: 0, top: elemOff - classNames[4], behavior: 'smooth'})
+    if (classNames[scrPos + 2] == 'jsmooth') {
+        if (classNames[scrPos + 3] == 'jsetpx') {
+            window.scrollTo({left: 0, top: elemOff - classNames[scrPos + 4], behavior: 'smooth'})
         } else {
             window.scrollTo({left: 0, top: elemOff, behavior: 'smooth'})
         }
-    } else if (classNames[2] == 'jsetpx') {
-        window.scrollTo(0, elemOff - classNames[3])
+    } else if (classNames[scrPos + 2] == 'jsetpx') {
+        window.scrollTo(0, elemOff - classNames[scrPos + 3])
     } else {
         window.scrollTo(0, elemOff)
     }
